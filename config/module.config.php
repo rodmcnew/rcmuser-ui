d68aed506fc3cbc9ee2daa213dce3715b3a3f6cf
James Jervis <jjervis@relivinc.com>
2015-12-18 17:37:45 -0600

    Update

--- config/module.config.php
+++ config/module.config.php
@@ -1,11 +1,27 @@
 <?php
 return [
     /**
+     * Config for allowing dynamic loading of public assets
+     */
+    'asset_manager' => [
+        'resolver_configs' => [
+            'aliases' => [
+                'modules/rcm-user/' => __DIR__ . '/../public/',
+            ],
+            // @todo implement this
+            //'collections' => [
+            //    'modules/rcm-user/rcm-user.js' => [
+            //    ],
+            //    'modules/admin/rcm-user/rcm-user.js' => [
+            //    ],
+            //],
+        ],
+    ],
+    /**
      * Configuration
      */
     'RcmUser\\Ui' => [
     ],
-
     'service_manager' => [
         'factories' => [
 
